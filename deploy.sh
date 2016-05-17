309246ec605087aae508a73a073392a6e5434e75
Rengaraj <sakthirengaraj@gmail.com>
2016-05-17 16:00:28 +0530

    rules for deleting existing pages in github.io are added

--- deploy.sh
+++ deploy.sh
@@ -13,18 +13,18 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     fi
 
     # # Using token clone gh-pages branch
-    # git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
+    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
 
-    # # Go into directory and copy data we're interested in to that directory
-    # cd built_website
-    # rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .
+    # Go into directory and copy data we're interested in to that directory
+    cd built_website
+    rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .
 
-    # echo -e "Remove previous version of website\n"
-    # git rm -rf .
-    # git clean -f -d
-    # git commit -m "Rel 1.5 - Empty the branch before pushing($TRAVIS_BUILD_NUMBER)"
-    # git push -fq origin $BRANCH > /dev/null
-    # cd ..
+    echo -e "Remove previous version of website\n"
+    git rm -rf .
+    git clean -f -d
+    git commit -m "Rel 1.5 - Empty the branch before pushing($TRAVIS_BUILD_NUMBER)"
+    git push -fq origin $BRANCH > /dev/null
+    cd ..
 
     echo -e "Starting deployment on Github Pages\n"
     # Using token clone gh-pages branch
