eed9fa8d247cb8d61436566c702375e1d43f0a11
Rengaraj <sakthirengaraj@gmail.com>
2016-05-17 17:26:26 +0530

    deploy script is modified

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 BRANCH=master
 TARGET_REPO=gnurenga/gnurenga.github.io.git
 PELICAN_OUTPUT_FOLDER=output
@@ -11,20 +12,6 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         git config --global user.email "gnurenga@outlook.com"
         git config --global user.name "Rengaraj"
     fi
-
-    # # Using token clone gh-pages branch
-    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
-
-    # Go into directory and copy data we're interested in to that directory
-    cd built_website
-    rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .
-
-    echo -e "Remove previous version of website\n"
-    git rm -rf .
-    git clean -f -d
-    git commit -m "Rel 1.5 - Empty the branch before pushing($TRAVIS_BUILD_NUMBER)"
-    git push -fq origin $BRANCH > /dev/null
-    cd ..
 
     echo -e "Starting deployment on Github Pages\n"
     # Using token clone gh-pages branch
